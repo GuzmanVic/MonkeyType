b73598dcc918c83a0fa795518d74bacedaa723fd
victor <vguzmanloredo@gmail.com>
2024-02-22 23:36:14 -0600

    mejoras

--- jaibascript.js
+++ jaibascript.js
@@ -4,22 +4,26 @@ const nextButton = document.getElementById("next");
 const textoInput = document.getElementById("texto");//TextArea con el texto a escribir
 // Conjunto de frases
 const frases = [
-    "No es lo mismo 'huele a traste', que 'atras te huele'",
-    "Que bueno que se hizo noche, para empezar la loquera",
-    "Una vez al año, no hace daño. Una vez al mes, que rico es. Una vez a la semana, van por tu hermana. Una vez al dia, que alegria. Una a cada rato, se te muere el aparato",
+    "No es lo mismo 'huele a traste' que 'atrás te huele'",
+    "Qué bueno que se hizo noche, para empezar la loquera",
+    "Una vez al año, no hace daño. Una vez al mes, qué rico es. Una vez a la semana, van por tu hermana. Una vez al día, qué alegría. Una a cada rato, se te muere el aparato",
     "Aguas de las verdes matas",
+    "Si yo fuera gay, yo podría enfundar tu pistola, te podría sacar la ponzoña... con todo y agijón",
     "Pujidos entre las palmas",
-    "Amiga date cuenta",
-    "la fogata, la escalera al cielo, el llanero solitario, la fantasma",
-    "El taladro del aberno, el fetuccini, el DJ, el hombre araña, el dedo del gloton",
-    "La inteligencia me persigue, pero yo soy mas rapido",
-    "Llego papa",
-    "Hoy se caso el huitlacoche",
-    "¿apoco si tilin?",
-    "Para mi el 'pollo loco' es un 'pollo normal'",
+    "Amiga, date cuenta",
+    "La fogata, la escalera al cielo, el llanero solitario, la fantasía",
+    "El taladro del averno, el fetuccini, el DJ, el hombre araña, el dedo del glotón",
+    "La inteligencia me persigue, pero yo soy más rápido",
+    "Llegó papá",
+    "Hoy se casó el huitlacoche",
+    "¿Apoco sí, tilín?",
+    "NOS VAMOS A MONTERREY!, NOS VAMOS A MONTERREY!!!",
+    "Para mí, el 'pollo loco' es un 'pollo normal'",
+    "Para mí, una locomotora, es una normalotora",
     "Hombres desnudos *buscar*",
-    "EEEERRRRneeestoooooOoooo",
-    "Mas vale pajaro en mano, que ser padre a los 15"
+    "EEeeEERRRRneeeeeeeeeEEEeeEEEstoooOOOoOoOoOOooooOoooo",
+    "Más vale pájaro en mano, que ser padre a los 15"
+
     // Agrega más frases según sea necesario
 ];
 
