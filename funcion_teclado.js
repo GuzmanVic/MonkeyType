b873d264ecca401352987b709b00e001e3a2eb10
/Bimori <106877704+Bimori404@users.noreply.github.com>
2024-02-26 20:47:17 -0600

    Bugs de las letras resuelto

--- funcion_teclado.js
+++ funcion_teclado.js
@@ -8,6 +8,7 @@ function handleKeyDown(event) {
     });
 
     switch (event.key) {
+        /* Sector Mano Izquierda */
         case '1':
         case '!':
             handleKeyHighlight('key-1');
@@ -27,29 +28,6 @@ function handleKeyDown(event) {
         case '5':
         case '%':
             handleKeyHighlight('key-5');
-            break;
-        case '6':
-        case '&':
-            handleKeyHighlight('key-6');
-            break;
-        case '7':
-        case '/':
-            handleKeyHighlight('key-7');
-            break;
-        case '8':
-        case '(':
-            handleKeyHighlight('key-8');
-            break;
-        case '9':
-        case ')':
-            handleKeyHighlight('key-9');
-            break;
-        case '0':
-        case '=':
-            handleKeyHighlight('key-0');
-            break;
-        case 'Tab':
-            handleKeyHighlight('key-tab');
             break;
         case 'q':
         case 'Q':
@@ -71,37 +49,6 @@ function handleKeyDown(event) {
         case 'T':
             handleKeyHighlight('key-t');
             break;
-        case 'y':
-        case 'Y':
-            handleKeyHighlight('key-y');
-            break;
-        case 'u':
-        case 'U':
-            handleKeyHighlight('key-u');
-            break;
-        case 'i':
-        case 'I':
-            handleKeyHighlight('key-i');
-            break;
-        case 'o':
-        case 'O':
-            handleKeyHighlight('key-o');
-            break;
-        case 'p':
-        case 'P':
-            handleKeyHighlight('key-p');
-            break;
-        case '[':
-        case '{':
-            handleKeyHighlight('key-open-bracket');
-            break;
-        case ']':
-        case '}':
-            handleKeyHighlight('key-close-bracket');
-            break;
-        case 'Enter':
-            handleKeyHighlight('key-enter');
-            break;
         case 'a':
         case 'A':
             handleKeyHighlight('key-a');
@@ -121,34 +68,6 @@ function handleKeyDown(event) {
         case 'g':
         case 'G':
             handleKeyHighlight('key-g');
-            break;
-        case 'h':
-        case 'H':
-            handleKeyHighlight('key-h');
-            break;
-        case 'j':
-        case 'J':
-            handleKeyHighlight('key-j');
-            break;
-        case 'k':
-        case 'K':
-            handleKeyHighlight('key-k');
-            break;
-        case 'l':
-        case 'L':
-            handleKeyHighlight('key-l');
-            break;
-        case ';':
-        case ':':
-            handleKeyHighlight('key-comma');
-            break;
-        case '\'':
-        case '"':
-            handleKeyHighlight('key-apos');
-            break;
-        case '\\':
-        case '|':
-            handleKeyHighlight('key-lees-grater');
             break;
         case 'z':
         case 'Z':
@@ -170,6 +89,88 @@ function handleKeyDown(event) {
         case 'B':
             handleKeyHighlight('key-b');
             break;
+
+        /* Sector Mano Derecha */
+        case '6':
+        case '&':
+            handleKeyHighlight('key-6');
+            break;
+        case '7':
+        case '/':
+            handleKeyHighlight('key-7');
+            break;
+        case '8':
+        case '(':
+            handleKeyHighlight('key-8');
+            break;
+        case '9':
+        case ')':
+            handleKeyHighlight('key-9');
+            break;
+        case '0':
+        case '=':
+            handleKeyHighlight('key-0');
+            break;
+        case "'":
+        case '?':
+            handleKeyHighlight('key-apos');
+            break;
+        case '¡':
+        case '¿':
+            handleKeyHighlight('key-question');
+            break;
+        case 'y':
+        case 'Y':
+            handleKeyHighlight('key-y');
+            break;
+        case 'u':
+        case 'U':
+            handleKeyHighlight('key-u');
+            break;
+        case 'i':
+        case 'I':
+            handleKeyHighlight('key-i');
+            break;
+        case 'o':
+        case 'O':
+            handleKeyHighlight('key-o');
+            break;
+        case 'p':
+        case 'P':
+            handleKeyHighlight('key-p');
+            break;
+        case '*':
+        case '+':
+            handleKeyHighlight('key-asterisk');
+            break;
+        case ']':
+        case '}':
+            handleKeyHighlight('key-close-bracket');
+            break;
+        case 'h':
+        case 'H':
+            handleKeyHighlight('key-h');
+            break;
+        case 'j':
+        case 'J':
+            handleKeyHighlight('key-j');
+            break;
+        case 'k':
+        case 'K':
+            handleKeyHighlight('key-k');
+            break;
+        case 'l':
+        case 'L':
+            handleKeyHighlight('key-l');
+            break;
+        case 'ñ':
+        case 'Ñ':
+            handleKeyHighlight('key-ñ');
+            break;
+        case '[':
+        case '{':
+            handleKeyHighlight('key-open-bracket');
+            break;
         case 'n':
         case 'N':
             handleKeyHighlight('key-n');
@@ -179,41 +180,50 @@ function handleKeyDown(event) {
             handleKeyHighlight('key-m');
             break;
         case ',':
-        case '<':
+        case ';':
             handleKeyHighlight('key-comma');
             break;
         case '.':
-        case '>':
+        case ':':
             handleKeyHighlight('key-dot');
             break;
-        case '/':
-        case '?':
-            handleKeyHighlight('key-question');
+        case '-':
+        case '_':
+            handleKeyHighlight('key-hyphen');
             break;
-        case 'Space':
-            handleKeyHighlight('key-space');
+
+        /* Teclas Especiales */
+        case 'Enter':
+            handleKeyHighlight('key-enter-up');
+            handleKeyHighlight('key-enter-down');
+            break;
+        case 'Control':
+            handleKeyHighlight('key-ctrl-left');
+            handleKeyHighlight('key-ctrl-right');
+            break;
+        case 'Alt':
+            handleKeyHighlight('key-alt-left');
+            handleKeyHighlight('key-alt-right');
+            break;
+        case ' ':
+            handleKeyHighlight('key-space-left');
+            handleKeyHighlight('key-space-right');
+            break;
+        case 'Shift':
+            handleKeyHighlight('key-shift-left');
+            handleKeyHighlight('key-shift-right');
             break;
         case 'CapsLock':
             handleKeyHighlight('key-caps-lock');
             break;
-        case 'Shift':
-            handleKeyHighlight('key-shift');
+        case 'Tab':
+            handleKeyHighlight('key-tab');
             break;
-        case 'Control':
-            handleKeyHighlight('key-ctrl');
+        case 'AltGraph':
+            handleKeyHighlight('key-altgr');
             break;
-        case 'Meta':
-            handleKeyHighlight('key-win');
-            break;
-        case 'Alt':
-            handleKeyHighlight('key-alt');
-            break;
-        case 'Escape':
-            handleKeyHighlight('key-esc');
-            break;
-        case 'Backspace':
-            handleKeyHighlight('key-backspace');
-            break;
+
+        /* Default */
         default:
             console.log("Tecla no manejada: " + event.key);
             break;
